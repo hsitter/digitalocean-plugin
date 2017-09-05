e03553674243fdcabf5d73e21745f784e415e324
Harald Sitter <sitter@kde.org>
2017-09-05 13:21:31 +0200

    track errors

--- src/main/java/com/dubture/jenkins/digitalocean/SlaveTemplate.java
+++ src/main/java/com/dubture/jenkins/digitalocean/SlaveTemplate.java
@@ -27,6 +27,7 @@
 package com.dubture.jenkins.digitalocean;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -127,6 +128,8 @@ public class SlaveTemplate implements Describable<SlaveTemplate> {
      */
     private final String initScript;
 
+    private LocalDateTime errorTime;
+
     private transient Set<LabelAtom> labelSet;
 
     private static final Logger LOGGER = Logger.getLogger(SlaveTemplate.class.getName());
@@ -170,6 +173,8 @@ public class SlaveTemplate implements Describable<SlaveTemplate> {
 
         this.userData = userData;
         this.initScript = initScript;
+
+        this.errorTime = null;
 
         readResolve();
     }
@@ -507,6 +512,19 @@ public class SlaveTemplate implements Describable<SlaveTemplate> {
         return initScript;
     }
 
+    public Boolean isErroring() {
+        if (null == errorTime) {
+            return false;
+        }
+        final LocalDateTime now = LocalDateTime.now();
+        if (now.isAfter(errorTime.plusHours(1))) {
+            // If an hour has passed since the error, consider the template not broken again.
+            errorTime = null;
+            return false;
+        }
+        return true;
+    }
+
     public int getSshPort() {
         return sshPort;
     }
@@ -524,5 +542,9 @@ public class SlaveTemplate implements Describable<SlaveTemplate> {
     protected Object readResolve() {
         labelSet = Label.parse(labels);
         return this;
+    }
+
+    public String toString() {
+        return getName();
     }
 }
